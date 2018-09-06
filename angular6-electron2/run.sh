5d1b75d65c224ef17068d6d1139b01597af70472
Tom Weatherhead <tom_weatherhead@hotmail.com>
2018-09-06 12:49:21 -0600

    angular6-electron2 : Got the audio working inside Docker

--- angular6-electron2/run.sh
+++ angular6-electron2/run.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
+
 xhost local:root
+
 # docker run --rm -m 512m --security-opt seccomp=unconfined -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY angular6-electron2
 
 USER_UID=$(id -u)
 
-docker run -t -i --rm -m 512m \
+# docker run -t -i --rm -m 512m ...
+#   ... angular6-electron2:latest /bin/bash
+
+docker run --rm -m 512m \
   --security-opt seccomp=unconfined \
-  -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -v /run/user/${USER_UID}/pulse:/run/user/1000/pulse \
+  --group-add $(getent group audio | cut -d: -f3) \
   -e DISPLAY=$DISPLAY \
+  -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v /run/user/${USER_UID}/pulse:/run/user/1001/pulse \
+  -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native \
+  -v ~/.config/pulse/cookie:/root/.config/pulse/cookie \
   angular6-electron2:latest
