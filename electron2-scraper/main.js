8a0f4c31bc54f092b813a014298185b99ad1bf76
Tom Weatherhead <tomw@2hrd4u.org>
2019-08-08 11:17:24 -0600

    Dev

--- electron2-scraper/main.js
+++ electron2-scraper/main.js
@@ -1,3 +1,5 @@
+const os = require('os');
+
 // Modules to control application life and create native browser window
 const { app, BrowserWindow, ipcMain, Tray } = require('electron');
 
@@ -46,21 +48,38 @@ ipcMain.on('consoleError', function(event, data) {
 	console.error(data);
 });
 
+function isPlatformWindows () {
+	const platform = os.platform();
+
+	//console.log('platform is', platform);
+
+	//const isWindows = platform === 'win32';
+	return platform === 'win32';
+}
+
 function createWindow () {
-	//const debug = true;
+	// const debug = true;
 	const debug = false;
-	//const isWindows = true;
-	const isWindows = false;
+	// const isWindows = true;
+	const isWindows = isPlatformWindows();
 	const windowWidth = debug ? 1000 : 435;
 	const windowHeight = debug ? 500 : 35;
 	const margin = 20;
+	const faviconFilename = isWindows ? 'favicon.ico' : 'favicon1_32x32.png';
 
 	// 45 is an estimate of the height of the Windows taskbar.
 	// -> 40px ? See https://www.reddit.com/r/Windows10/comments/6rv7ot/on_a_1920x1080_display_at_native_resolution_how/
 	const windows10TaskbarHeight = isWindows ? 40 : 0;
 
 	// Create the browser window:
-	mainWindow = new BrowserWindow({width: windowWidth, height: windowHeight, frame: false});
+	mainWindow = new BrowserWindow({
+		width: windowWidth,
+		height: windowHeight,
+		// frame: false,
+		frame: debug,
+		backgroundColor: '#ffffff',
+		icon: 'assets/' + faviconFilename
+	});
 
 	//console.log('mainWindow is', mainWindow);
 
@@ -89,10 +108,9 @@ function createWindow () {
 
 	console.log(`Primary display: ${screenDimensions.width} x ${screenDimensions.height}`);
 
-	mainWindow.setPosition(screenDimensions.width - windowWidth - margin, screenDimensions.height - windowHeight - windows10TaskbarHeight - margin);
+	// mainWindow.setPosition(screenDimensions.width - windowWidth - margin, screenDimensions.height - windowHeight - windows10TaskbarHeight - margin);
 
 	// For best results on Windows, use an .ico file. See https://electronjs.org/docs/api/tray .
-	const faviconFilename = isWindows ? 'favicon.ico' : 'favicon1_32x32.png';
 	//const tray = new Tray('./assets/favicon.ico'); // Windows 10
 	//const tray = new Tray('./assets/favicon1_32x32.png'); // Ubuntu 18.04
 	const tray = new Tray('./assets/' + faviconFilename);
